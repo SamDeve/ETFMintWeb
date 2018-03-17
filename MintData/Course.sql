0872811a45f6b2327cbe8cca4db82a684689f47e
Manuj Sarpal <manujsarpal@hotmail.com>
2018-03-17 17:00:47 +0000

    Initial code

---
+++ MintData/Course.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Course] (
+    [CourseID] INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NULL,
+    [Credits]  INT           NULL,
+    [IsActive] SMALLINT NULL DEFAULT 1, 
+    [IsDeleted] SMALLINT NULL DEFAULT 1, 
+    [CreatedOn] DATETIME NULL DEFAULT GetDate()  , 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME NULL DEFAULT GetDate() , 
+    [ModifiedBy] NVARCHAR(50) NULL, 
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
---
+++ MintWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MintWeb.Startup))]
+namespace MintWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
